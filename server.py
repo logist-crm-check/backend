0871358e48af9044c469a4f581f00198684c365b
Andre.Balakirev <andre.balakirev@mail.ru>
2025-04-05 14:55:06 +0300

    Добавил druno

--- server.py
+++ server.py
@@ -1,25 +1,19 @@
-from flask import Flask
+from flask import Flask,request
 from weather import weather_by_city
 app = Flask(__name__)
 
 
-@app.route('/')
+@app.post('/')
 def index():
-    weather = weather_by_city("Moscow,Russia")
+    body = request.get_json()
+    city = body["city"]
+    weather = weather_by_city(city)
     if weather:
        weather_text = f"Сейчас {weather['temp_C']}, ощущается как {weather['FeelsLikeC']}"
     else:
        weather_text = "Прогноз сейчас недоступен"
 
-    return f"""<html>
-    <head>
-        <title>Прогноз погоды</title>
-        </head>
-        <body>
-        <h1>{weather_text}</h1>
-    </body>
-</html>"""
-
+    return {"temp_c": weather['temp_C'], "feels": weather['FeelsLikeC']}
 
 if __name__=="__main__":
     app.run()
